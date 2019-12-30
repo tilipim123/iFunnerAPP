86a3b5931f480f836f32f3db824567d1e75d6cfb
Caio Felipe <caio_tilipus@hotmail.com>
2019-12-30 15:34:17 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class FlutterGames extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'iFunner',
+      title: 'iFunnerAPP',
       theme: defaultTheme,
       home: MainPage(),
       debugShowCheckedModeBanner: false,
