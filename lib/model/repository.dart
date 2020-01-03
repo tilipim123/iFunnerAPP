88b60d8fe098e22cfe0f7ac72a21aac80994d875
rafaeloliveira <derafael04@gmail.com>
2020-01-02 22:35:52 -0300

    Juste a imagem de fundo

--- lib/model/repository.dart
+++ lib/model/repository.dart
@@ -10,186 +10,186 @@ final List<Game> newGamesList = [
 final List<Game> popularGamesList = [codww2, fallout4, fc5, pvzgw2, riseoftr];
 
 final Game pvzgw2 = Game(
-  name: '9',
+  name: 'Serviço 9',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
-      'https://imgur.com/OIJ7v0V',
+      'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'The battle for suburbia grows to crazy new heights! In this hilarious, action-packed shooter, zombie leader Dr. Zomboss has strengthened his horde and rebuilt suburbia as a zombie utopia. But hope remains, because for the first time the plants are taking the offensive in an all-out attack to reclaim their turf. Play as the plants in the all-new 24-player Herbal Assault mode, or choose your side in 4-player co-op or Solo Play!',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 4,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/pvzgw2_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/pvzgw2_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/pvzgw2_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/pvzgw2_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game codww2 = Game(
-  name: '6',
+  name: 'Serviço 6',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'Step back in time with Call of Duty WWII. Land in Normandy on D-Day and battle across Europe through iconic locations in history’s most monumental war. Experience classic Call of Duty combat, the bonds of camaraderie, and the unforgiving nature of war. Go at it alone or grab your closest war buddies and take the battle online!',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 3.6,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/codww2_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/codww2_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/codww2_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/codww2_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game fc5 = Game(
-  name: '8',
+  name: 'Serviço 8',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'Whether through coercion, starvation or forced baptisms, the cult will use every resource available to build a following in preparation of The Collapse. Committed to restoring freedom to Hope County, The Resistance has rallied together in order to stop the Father and his followers. And they’ll only succeed with your help.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 4.5,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fc5_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fc5_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fc5_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fc5_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game riseoftr = Game(
-  name: '10',
+  name: 'Serviço 10',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'In Rise of the Tomb Raider, Lara uncovers an ancient mystery that places her in the cross-hairs of a ruthless organization. Featuring epic, high-octane action moments set in the most beautiful hostile environments on earth, Rise of the Tomb Raider delivers a cinematic survival action adventure where you will join Lara Croft on her first tomb raiding expedition as she seeks to discover the secret of immortality.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 4.3,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/rotr_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/rotr_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/rotr_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/rotr_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpgg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game detroitbh = Game(
-    name: '2',
+    name: 'Serviço 2',
     box:
         'https://imgur.com/79WHBIc.jpg',
     cover:
         'https://imgur.com/OIJ7v0V.jpg',
     description:
-        'Travel to the near-future metropolis of Detroit – a city rejuvenated by an exciting technological development: androids. Witness your brave new world turn to chaos as you take on the role of Kara, a female android trying to find her own place in a turbulent social landscape.',
+    'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
     platforms: [
       '',
     ],
     rating: 4,
     screenshots: [
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_1.jpg',
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_2.jpg',
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_3.jpg',
-      'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/detroitbh_4.jpg',
+      'https://imgur.com/OIJ7v0V.jpg',
+      'https://imgur.com/OIJ7v0V.jpg',
+      'https://imgur.com/OIJ7v0V.jpg',
+      'https://imgur.com/OIJ7v0V.jpg',
     ]);
 
 final Game shadowcolossus = Game(
-  name: '3',
+  name: 'Serviço 3',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'Armed with only a sword and a bow, journey through ancient lands to seek out gigantic beasts.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 4.5,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/shadowcolossus_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/shadowcolossus_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/shadowcolossus_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/shadowcolossus_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game agentsofmayhem = Game(
-  name: '4',
+  name: 'Serviço 4',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'In Agents of Mayhem, a nefarious plot is set in motion to build a world-altering device that will grant unrivaled power to the supervillain organization known as LEGION. Pick your squad of 3 highly skilled and iconic characters from a roster of 12 uniquely designed MAYHEM agents and get ready to save the world!',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 3.6,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/agentsofmayhem_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/agentsofmayhem_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/agentsofmayhem_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/agentsofmayhem_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game fallout4 = Game(
-  name: '7',
+  name: 'Serviço 7',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'As the sole survivor of Vault 111, you enter a world destroyed by nuclear war. Every second is a fight for survival, and every choice is yours. Only you can rebuild and determine the fate of the Wasteland. Welcome home.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 4.2,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fallout_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fallout_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fallout_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/fallout_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game mgsurvive = Game(
-  name: '5',
+  name: 'Serviço 5',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
       'https://imgur.com/OIJ7v0V.jpg',
   description:
-      'Create your own character and learn to survive. Scavenge resources, craft weapons, build a base camp and explore the unknown while developing the survival skills necessary in this hostile environment.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
   rating: 3,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/mgsurvive_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/mgsurvive_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/mgsurvive_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/mgsurvive_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
 final Game seaofthieves = Game(
-  name: '1',
+  name: 'Serviço 1',
   box:
       'https://imgur.com/79WHBIc.jpg',
   cover:
@@ -201,10 +201,10 @@ final Game seaofthieves = Game(
   ],
   rating: 3.3,
   screenshots: [
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/seaofthieves_1.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/seaofthieves_2.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/seaofthieves_3.jpg',
-    'https://raw.githubusercontent.com/searchy2/FlutterGames/master/images/screenshot/seaofthieves_4.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
+    'https://imgur.com/OIJ7v0V.jpg',
   ],
 );
 
@@ -214,7 +214,7 @@ final Game godofwar = Game(
       'https://imgur.com/79WHBIc.jpg',
   cover: '',
   description:
-      'Living as a man outside the shadow of the gods, Kratos must adapt to the unfamiliar Norse lands, unexpected threats, and a second chance at being a father.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
@@ -233,7 +233,7 @@ final Game monsterhunterw = Game(
       'https://imgur.com/79WHBIc.jpg',
   cover: '',
   description:
-      'Monster Hunter: World puts you in a living, breathing ecosystem as a hunter that seeks and slays ferocious beasts in heart-pounding battles that unfold across the vast ever-changing terrain. Venture on quests alone or cooperatively with up to three hunters via online drop-in multiplayer.',
+  'Lorem ipsum luctus torquent mollis vulputate fermentum luctus hendrerit sodales massa, non habitasse id consectetur aliquam dui non curabitur dolor vitae, duis non fames mollis lectus eget per lorem magna.',
   platforms: [
     '',
   ],
