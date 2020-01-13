99fc6f09c6dad9a25ecebe8caf2d9b81d30c2335
rafaeloliveira <derafael04@gmail.com>
2020-01-12 23:08:18 -0300

    ajsute categoria

--- lib/page/busca/buscas_categorias.dart
+++ lib/page/busca/buscas_categorias.dart
@@ -1,26 +1,13 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'Wigets/categoria_tile.dart';
 
 class Categoria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<QuerySnapshot>(
-      future: Firestore.instance.collection("Categorias").getDocuments(),
-      builder: (context, snapshot){
-        if(!snapshot.hasData) return Center(
-        child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation(Colors.deepOrangeAccent),
-        ),
-        );
-        return ListView.builder(
-          itemCount: snapshot.data.documents.length,
-          itemBuilder: (context, index){
-            return CategoriaTile(snapshot.data.documents[index]);
-          },
-        );
-      },
+    return Container(
+      child: Card(
+
+      ),
     );
   }
 }
