62da0aaba812e1ae5509543d7d308eaf18af0e65
rafaeloliveira <derafael04@gmail.com>
2020-01-15 23:45:41 -0300

    tela do usuario

--- lib/page/user/Screen User/Wigets/page_myitens.dart
+++ lib/page/user/Screen User/Wigets/page_myitens.dart
@@ -1,10 +1,56 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Myitens extends StatelessWidget {
+class Myuser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: ListView(
+        padding: EdgeInsets.all(10),
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.shopping_cart),
+            title: Text('Compras',
+              style: TextStyle(color: Colors.black87),
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.credit_card),
+            title: Text('Formas de Pagamento',
+            style: TextStyle(color: Colors.black87),
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.location_on),
+            title: Text('Endereço',
+              style: TextStyle(color: Colors.black87),
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Configurações',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.help),
+            title: Text('Ajuda',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onTap: (){},
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text('Notificação',
+              style: TextStyle(color: Colors.black38),
+            ),
+            onTap: (){},
+          ),
+        ],
       ),
     );
   }
--- lib/page/user/Screen User/page_user.dart
+++ lib/page/user/Screen User/page_user.dart
@@ -2,51 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'Wigets/page_myitens.dart';
 
-class Screenuser extends StatefulWidget {
-  @override
-  _ScreenuserState createState() => _ScreenuserState();
-}
-
-class _ScreenuserState extends State<Screenuser> {
-
+class Screenuser extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(16),
-            height: 100,
-            width: 300,
-            child: Column(
-              children: <Widget>[
-                Icon(
-                  Icons.person_outline,
-                  color: Colors.deepOrangeAccent,
-                  size: 40,
-                ),
-                Text("Fulando da Silva Costa",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black54,
-                  ),
-                ),
-                GestureDetector(
-                  child: Text(
-                    "Editar",
-                    style: TextStyle(
-                      color: Colors.deepOrangeAccent,
-                      fontSize: 15,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                  onTap: (){
-
-                  },
-                )
-              ],
-            ),
-        ),
-      ),
+    return Container(
+      child: Myuser(),
     );
   }
 }
