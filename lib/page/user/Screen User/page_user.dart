b47e09965661bf4ee4c2b1dc9b960e018d63e4e4
rafaeloliveira <derafael04@gmail.com>
2020-03-19 16:44:04 -0300

    Criado nova tela do usuario

--- lib/page/user/Screen User/page_user.dart
+++ lib/page/user/Screen User/page_user.dart
@@ -1,12 +1,116 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'Wigets/page_myitens.dart';
 
-class Screenuser extends StatelessWidget {
+class Screenuser extends StatefulWidget {
+  @override
+  _ScreenuserState createState() => _ScreenuserState();
+}
+
+class _ScreenuserState extends State<Screenuser> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Myuser(),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 250,
+            decoration: BoxDecoration(
+                color: Colors.grey[800],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(25),
+                    bottomRight: Radius.circular(25))),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: MediaQuery.of(context).viewPadding.top),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    height: 80,
+                    width: MediaQuery.of(context).size.width,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 232, 96, 75),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Icon(
+                            Feather.settings,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          width: 40,
+                          height: 40,
+                          decoration: BoxDecoration(
+                            color: Colors.grey[600],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Center(
+                            child: Text(
+                              '0',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.oxygen(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16),
+                          ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: Column(
+                    children: <Widget>[
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: Container(
+                            height: 80,
+                            width: 80,
+                            child: Image.asset("assets/ifunnerclaro.jpg"),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Fulado da Silva',
+                        style: GoogleFonts.oxygen(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Editar Perfil',
+                        style: GoogleFonts.oxygen(
+                            color: Colors.white, fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Myuser(),
+          )
+        ],
+      ),
     );
   }
 }
